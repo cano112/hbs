954d22cfeb7d84a3c2f5e62f606184cf7c35b19b
matr@grapeup.com <matr@grapeup.com>
2018-11-14 22:44:26 +0100

    shouldSend circular implementation

--- hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/CircularPropagation.scala
+++ hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/CircularPropagation.scala
@@ -6,4 +6,20 @@ class CircularPropagation(val center: Vector, val radius: Double) extends Propag
 
   override def shouldReceive(receiver: Agent): Boolean = center.distance(receiver.position()) < radius
 
+  override def shouldSend(pos1: Vector, pos2: Vector): Boolean = {
+    val x1 = pos1(0)
+    val y1 = pos1(1)
+    val x2 = pos2(0)
+    val y2 = pos2(1)
+    val a = y1 - y2
+    val b = x2 - x1
+    val c = (x1 - x2) * y1 + (y2 - y1) * x1
+
+    val x = center(0)
+    val y = center(1)
+    val dist = Math.abs(a * x + b * y + c) / Math.sqrt(a * a + b * b)
+
+    radius >= dist
+  }
+
 }
--- hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/Propagation.scala
+++ hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/Propagation.scala
@@ -1,9 +1,12 @@
 package pl.edu.agh.hbs.model.propagation
 
+import pl.edu.agh.hbs.model
 import pl.edu.agh.hbs.model.Agent
 
 abstract class Propagation extends Serializable {
 
   def shouldReceive(receiver: Agent): Boolean
+
+  def shouldSend(leftBottom: model.Vector, rightUpper: model.Vector): Boolean
 
 }
