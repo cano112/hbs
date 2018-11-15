2912adf2ddcbe32275cd733605c0aeebc0c09198
matr@grapeup.com <matr@grapeup.com>
2018-11-15 16:40:27 +0100

    shouldSend circular implementation fix

--- hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/CircularPropagation.scala
+++ hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/CircularPropagation.scala
@@ -1,16 +1,26 @@
 package pl.edu.agh.hbs.model.propagation
 
+
 import pl.edu.agh.hbs.model.{Agent, Vector}
 
 class CircularPropagation(val center: Vector, val radius: Double) extends Propagation {
 
   override def shouldReceive(receiver: Agent): Boolean = center.distance(receiver.position()) < radius
 
-  override def shouldSend(pos1: Vector, pos2: Vector): Boolean = {
-    val x1 = pos1(0)
-    val y1 = pos1(1)
-    val x2 = pos2(0)
-    val y2 = pos2(1)
+  def shouldSend(leftBottom: Vector, rightUpper: Vector): Boolean = {
+    val rightBottom = Vector(rightUpper(0), leftBottom(1))
+    val leftUpper = Vector(leftBottom(0), rightUpper(1))
+    crossLineAndCircle(leftBottom, leftUpper) ||
+      crossLineAndCircle(rightBottom, rightUpper) ||
+      crossLineAndCircle(rightBottom, leftBottom) ||
+      crossLineAndCircle(leftUpper, rightUpper)
+  }
+
+  private def crossLineAndCircle(p1: Vector, p2: Vector): Boolean = {
+    val x1 = p1(0)
+    val y1 = p1(1)
+    val x2 = p2(0)
+    val y2 = p2(1)
     val a = y1 - y2
     val b = x2 - x1
     val c = (x1 - x2) * y1 + (y2 - y1) * x1
--- hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/Propagation.scala
+++ hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/propagation/Propagation.scala
@@ -1,12 +1,11 @@
 package pl.edu.agh.hbs.model.propagation
 
-import pl.edu.agh.hbs.model
-import pl.edu.agh.hbs.model.Agent
+import pl.edu.agh.hbs.model.{Agent, Vector}
 
 abstract class Propagation extends Serializable {
 
   def shouldReceive(receiver: Agent): Boolean
 
-  def shouldSend(leftBottom: model.Vector, rightUpper: model.Vector): Boolean
+  def shouldSend(leftBottom: Vector, rightUpper: Vector): Boolean
 
 }
