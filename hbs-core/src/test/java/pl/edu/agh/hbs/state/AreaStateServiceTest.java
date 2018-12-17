91b5103edc719335a723ce009ec47406a46cc677
Kamil Noster <kamil.noster@gmail.com>
2018-12-17 23:07:27 +0100

    Add more AreaStateService tests

--- hbs-core/src/test/java/pl/edu/agh/hbs/state/AreaStateServiceTest.java
+++ hbs-core/src/test/java/pl/edu/agh/hbs/state/AreaStateServiceTest.java
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
 import pl.edu.agh.hbs.model.Vector;
+import pl.edu.agh.hbs.simulation.api.Area;
 import pl.edu.agh.hbs.simulation.api.AreaBordersDefinition;
 import pl.edu.agh.hbs.simulation.cartesian.CartesianRectangularBordersDefinition;
+import pl.edu.agh.hbs.simulation.generic.GenericArea;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
@@ -71,6 +71,162 @@ public class AreaStateServiceTest {
 
         // then
         then(area).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnAreaWithSharedBorderWhenBottomBorderShared() {
+        // given
+        final String matchingAreaId = "area-3";
+        final Area currentArea = getCurrentArea("area-1");
+        final Collection<Area> areas = Arrays.asList(
+                getNotMatchingArea("area-2"),
+                getSharedBottomBorderArea(matchingAreaId));
+
+        // when
+        final Collection<String> neighbourAreaIds = areaStateService.findNeighbourAreaIds(areas, currentArea);
+
+        // then
+        then(neighbourAreaIds).containsOnly(matchingAreaId);
+    }
+
+    @Test
+    public void shouldReturnAreaWithSharedBorderWhenTopBorderShared() {
+        // given
+        final String matchingAreaId = "area-3";
+        final Area currentArea = getCurrentArea("area-1");
+        final Collection<Area> areas = Arrays.asList(
+                getNotMatchingArea("area-2"),
+                getSharedTopBorderArea("area-3"));
+
+        // when
+        final Collection<String> neighbourAreaIds = areaStateService.findNeighbourAreaIds(areas, currentArea);
+
+        // then
+        then(neighbourAreaIds).containsOnly(matchingAreaId);
+    }
+
+    @Test
+    public void shouldReturnAreaWithSharedBorderWhenLeftBorderShared() {
+        // given
+        final String matchingAreaId = "area-3";
+        final Area currentArea = getCurrentArea("area-1");
+        final Collection<Area> areas = Arrays.asList(
+                getNotMatchingArea("area-2"),
+                getSharedLeftBorderArea("area-3"));
+
+        // when
+        final Collection<String> neighbourAreaIds = areaStateService.findNeighbourAreaIds(areas, currentArea);
+
+        // then
+        then(neighbourAreaIds).containsOnly(matchingAreaId);
+    }
+
+    @Test
+    public void shouldReturnAreaWithSharedBorderWhenRightBorderShared() {
+        // given
+        final String matchingAreaId = "area-3";
+        final Area currentArea = getCurrentArea("area-1");
+        final Collection<Area> areas = Arrays.asList(
+                getNotMatchingArea("area-2"),
+                getSharedRightBorderArea("area-3"));
+
+        // when
+        final Collection<String> neighbourAreaIds = areaStateService.findNeighbourAreaIds(areas, currentArea);
+
+        // then
+        then(neighbourAreaIds).containsOnly(matchingAreaId);
+    }
+
+    @Test
+    public void shouldReturnEmptyCollectionWhenMatchedWithItself() {
+        // given
+        final Area currentArea = getCurrentArea("area-1");
+        final Collection<Area> areas = Arrays.asList(getCurrentArea("area-1"), getCurrentArea("area-1"));
+
+        // when
+        final Collection<String> neighbourAreaIds = areaStateService.findNeighbourAreaIds(areas, currentArea);
+
+        // then
+        then(neighbourAreaIds).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnEmptyCollectionWhenNoAreaProvided() {
+        // given
+        final Area currentArea = getCurrentArea("area-1");
+
+        // when
+        final Collection<String> neighbourAreaIds = areaStateService
+                .findNeighbourAreaIds(Collections.emptyList(), currentArea);
+
+        // then
+        then(neighbourAreaIds).isEmpty();
+    }
+
+    private Area getCurrentArea(String areaId) {
+        final AreaBordersDefinition currentAreaBordersDefinition = new CartesianRectangularBordersDefinition(
+                Vector.of(50, 50),
+                Vector.of(100, 100),
+                false,
+                false,
+                false,
+                true);
+        return new GenericArea(areaId, null, currentAreaBordersDefinition, new LinkedList<>());
+    }
+
+    private Area getNotMatchingArea(String areaId) {
+        final AreaBordersDefinition areaBordersDefinition = new CartesianRectangularBordersDefinition(
+                Vector.of(200, 200),
+                Vector.of(300, 300),
+                false,
+                false,
+                false,
+                true);
+        return new GenericArea(areaId, null, areaBordersDefinition, new LinkedList<>());
+    }
+
+    private Area getSharedBottomBorderArea(String areaId) {
+        final AreaBordersDefinition areaBordersDefinition = new CartesianRectangularBordersDefinition(
+                Vector.of(50, 0),
+                Vector.of(100, 50),
+                false,
+                false,
+                true,
+                true);
+        return new GenericArea(areaId, null, areaBordersDefinition, new LinkedList<>());
+    }
+
+    private Area getSharedTopBorderArea(String areaId) {
+        final AreaBordersDefinition areaBordersDefinition = new CartesianRectangularBordersDefinition(
+                Vector.of(50, 100),
+                Vector.of(100, 200),
+                false,
+                false,
+                true,
+                true);
+        return new GenericArea(areaId, null, areaBordersDefinition, new LinkedList<>());
+    }
+
+    private Area getSharedLeftBorderArea(String areaId) {
+        final AreaBordersDefinition areaBordersDefinition = new CartesianRectangularBordersDefinition(
+                Vector.of(0, 50),
+                Vector.of(50, 100),
+                false,
+                false,
+                true,
+                true);
+        return new GenericArea(areaId, null, areaBordersDefinition, new LinkedList<>());
+    }
+
+    private Area getSharedRightBorderArea(String areaId) {
+        final AreaBordersDefinition areaBordersDefinition = new CartesianRectangularBordersDefinition(
+                Vector.of(100, 50),
+                Vector.of(150, 100),
+                false,
+                false,
+                true,
+                true);
+        return new GenericArea(areaId, null, areaBordersDefinition, new LinkedList<>());
     }
 
 }
